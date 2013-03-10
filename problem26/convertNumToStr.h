04d973e1118781235a644b5cdd3f6ab37afbbca0
Stymphalian <saturnslight@gmail.com>
2013-03-09 23:35:58 -0800

    Now it should work..

--- problem26/convertNumToStr.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONVERTNUMTOSTR_H
-#define CONVERTNUMTOSTR_H
-
-char* convertNumToStr(int num);
-
-#endif
--- problem30/convertNumToStr.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONVERTNUMTOSTR_H
-#define CONVERTNUMTOSTR_H
-
-char* convertNumToStr(int num);
-char* convertNumToStr_double(double num);
-
-#endif
